f0c7a78bf9a4af86413221e34b682582022e551c
Richard Edigin <richardedigin7988@gmail.com>
2026-08-05 18:02:13 +0100

    i tried to resolve a rendering issue with my code

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import { getIngredients } from "./ingredientInput.js";
-
+loadHeaderFooter();
 document.querySelector("#submit-btn").addEventListener("click", ()=>{
 	const ingredients = getIngredients();
 	console.log(ingredients);
 });
 
-loadHeaderFooter();
+
